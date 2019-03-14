160ad66121798e9109d578ec50a678499bac415f
PJvGrol <pjvgrol@xs4all.nl>
2019-03-14 11:50:16 +0100

    Update .gitignore, add test json

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -11,6 +11,7 @@ import Data.Map
 data PSettings = PSettings { dataType      :: Text,
                              graphType     :: Text, 
                              title         :: Maybe Text,
+                             values        :: Text,
                              properties    :: Maybe [Map Text Text],
                              outputType    :: Maybe Text } 
                              deriving Show
@@ -22,10 +23,11 @@ instance FromJSON PSettings where
     PSettings <$> v .:  "dataType"
               <*> v .:  "graphType" 
               <*> v .:? "title"
+              <*> v .:  "values"
               <*> v .:? "properties"
               <*> v .:? "outputType"
   parseJSON _ = error "Wrong structure of input file"                               
 
 --Parses a JSON file from a directory on your computer  
-main = do b <- Data.ByteString.Lazy.readFile "/Users/Ymke/Documents/Studie/afp/jsontest.json"
+main = do b <- Data.ByteString.Lazy.readFile "jsontest.json"
           return (decode b :: Maybe PSettings)

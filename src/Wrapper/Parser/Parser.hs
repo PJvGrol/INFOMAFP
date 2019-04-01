2e2da3ec3bfb819a956effd635d360964af1e343
Ymke Wegereef <Ymkewe@gmail.com>
2019-04-01 21:05:37 +0200

    Added functions for XML parsing and tests for XML files.

--- src/Wrapper/Parser/Parser.hs
+++ src/Wrapper/Parser/Parser.hs
@@ -1,12 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, BlockArguments #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Parser where
 
 import Data.Aeson
 import Data.Maybe
-import Data.ByteString.Lazy
-import Data.Map
+import qualified Data.ByteString.Lazy as B (readFile)
+import Data.Map (Map, fromList)
+import Text.XML.HXT.Parser.XmlParsec
+import Text.XML.HXT.DOM.XmlNode hiding (getText)
+import Text.XML.HXT.Core hiding (xread, getChildren, getText)
 
 data PSettings = PSettings { inputData     :: String,
                              graphType     :: String, 
@@ -25,5 +28,50 @@ instance FromJSON PSettings where
   parseJSON _ = error "Wrong structure of input file"                               
 
 --Parses a JSON file from a directory on your computer  
-main = do b <- Data.ByteString.Lazy.readFile "linePlotTest.json"
-          return (decode b :: Maybe PSettings)
+parJson = do b <- B.readFile "line_correct.json"
+             return (decode b :: Maybe PSettings)
+ 
+main = do b <- readFile "test.xml"
+          return (parseXML b)
+          
+parseXML b = let x = xread b in
+                if null x then Nothing else (ntreeToPsettings.removeEmpty.getChildren) (head x) 
+
+{- Checks if a string consists solely of whitespaces -}
+isEmpty :: String -> Bool
+isEmpty ls = foldr ((&&).(\x -> x == ' ' || x == '\n' || x == '\t')) True ls
+
+{- Removes all empty text fields from a NTree -} 
+removeEmpty :: [NTree XNode] -> [NTree XNode] 
+removeEmpty []   = []
+removeEmpty (x:xs) = case x of (NTree (XText s) _)    -> if isEmpty s then removeEmpty xs else x : (removeEmpty xs)
+                               (NTree (XTag s []) ls) -> (NTree (XTag s []) (removeEmpty ls)) : (removeEmpty xs)
+                               _                      -> x : (removeEmpty xs)
+
+getText (NTree (XText s) _) = s   
+getLabel s (NTree (XTag label _) _) = label     
+
+{- Finds the right tag corresponding to the input string -}
+findTag :: String -> [NTree XNode] -> Maybe [NTree XNode]
+findTag s [] = Nothing
+findTag s ((NTree (XTag label _) ls):xs) = if (localPart label) == s then Just ls else findTag s xs                          
+    
+parseProp :: [NTree XNode] -> [(String,String)]
+parseProp []                         = []
+parseProp ((NTree (XTag l _) ls):xs) = (localPart l, getText (head ls)) : (parseProp xs)
+
+{- Changes the NTree that comes from a XML file into the PSettings type -}
+ntreeToPsettings :: [NTree XNode] -> Maybe PSettings
+ntreeToPsettings [] = Nothing
+ntreeToPsettings t = let x1 = findTag "inputData" t
+                         x2 = findTag "graphType" t in
+                       if x1 == Nothing then Nothing else
+                          if x2 == Nothing then Nothing else Just
+                            (PSettings (getText (head (fromJust x1))) 
+                                      (getText (head (fromJust x2)))
+                                      (case findTag "title" t of Just x -> Just (getText (head x))
+                                                                 _      -> Nothing)
+                                      (case findTag "properties" t of Just x -> Just (fromList (parseProp x))
+                                                                      _      -> Nothing)
+                                      (case findTag "outputType" t of Just x -> Just (getText (head x))
+                                                                      _      -> Nothing))
--- test/TestValidator.hs
+++ test/TestValidator.hs
@@ -6,7 +6,7 @@ import Test.HUnit
 import Validator
 import Parser
 import Data.Aeson
-import Data.ByteString.Lazy
+import qualified Data.ByteString.Lazy as B
 import ErrorData
 
 main = do runTestTT tests
@@ -16,47 +16,71 @@ parseFile b = case (decode b :: Maybe PSettings) of Nothing -> Errors [RequiredF
                                                     Just v  -> case parse v of Left e -> e
                                                                                _      -> Errors [NoErrorsFound "Found no errors"]
                                                                                
+parseXML' b = case parseXML b of Nothing -> Errors [RequiredFieldMissing "One of the required fields has incorrect input or is missing."]
+                                 Just v -> case parse v of Left e -> e 
+                                                           _      -> Errors [NoErrorsFound "Found no errors"]
+                                                                               
 {- All tests in this file bundled together -}
 tests = TestList [TestLabel "General tests" general_tests,
-                  TestLabel "Line plot tests" lineplot_tests]
+                  TestLabel "Line plot tests" lineplot_tests,
+                  TestLabel "XML tests" xml_tests]
                                                                                                                                                             
-general_tests = TestList [TestLabel "Test empty" test_empty, 
-                          TestLabel "Test missing" test_missing,
-                          TestLabel "Test unrecognized" test_unrec
-                         ]     
+general_tests  = TestList [TestLabel "Test empty" test_empty, 
+                           TestLabel "Test missing" test_missing,
+                           TestLabel "Test unrecognized" test_unrec
+                           ]   
+                         
+xml_tests      = TestList [TestLabel "Test lines" xml_correct,
+                           TestLabel "Test empty" xml_empty,
+                           TestLabel "Test missing" xml_missing
+                           ]
                          
 lineplot_tests = TestList [TestLabel "Test line plot mult. errors" line_mult,
                            TestLabel "Test unrecognized vals line plot" lines_val,
                            TestLabel "Test correct file" line_correct
-                           ]
+                            ]
  
 ---------- General tests -----------                           
 
 {- Tests the output of an empty file -}
-test_empty = TestCase (do b <- Data.ByteString.Lazy.readFile "general/empty.json"
+test_empty = TestCase (do b <- B.readFile "general/empty.json"
                           assertEqual "Empty file" (decode b :: Maybe PSettings) Nothing)
                           
 {- Tests an input file where one of the mandatory fields is missing. -} 
-test_missing = TestCase (do b <- Data.ByteString.Lazy.readFile "general/missing_graphtype.json"
+test_missing = TestCase (do b <- B.readFile "general/missing_graphtype.json"
                             assertEqual "Missing graph type" (decode b :: Maybe PSettings) Nothing)
                             
 {- Tests an input file with unrecognized input one of the mandatory fields. 
    (in this case the graph type) -}
-test_unrec = TestCase (do b <- Data.ByteString.Lazy.readFile "general/unrecognized_graphtype.json"
+test_unrec = TestCase (do b <- B.readFile "general/unrecognized_graphtype.json"
                           assertEqual "Unrecognized graph type" (parseFile b) (Errors [UnknownGraphType "The provided graph type is not recognized."]))
  
                           
 ---------- Line plot tests -----------
                           
 {- Tests an input file with no incorrect entries that is completely filled -}
-line_correct = TestCase (do b  <- Data.ByteString.Lazy.readFile "line_plot/line_correct.json"
+line_correct = TestCase (do b  <- B.readFile "line_plot/line_correct.json"
                             assertEqual "correct" (parseFile b) (Errors [NoErrorsFound "Found no errors"]))
 
 {- An input file for a line plot with multiple errors. -}
-line_mult = TestCase (do b  <- Data.ByteString.Lazy.readFile "line_plot/linePlotTest.json"
+line_mult = TestCase (do b  <- B.readFile "line_plot/linePlotTest.json"
                          assertEqual "mult" (parseFile b) t )
                             where t = Errors [(DataNotMatchesType "The provided limit values for a line plot do not match the correct type. The desired type is [[(x,y)]]."), (UnknownOutPutType "The provided output type is not recognized.")]           
 
 {- Tests an input file that has input values of the wrong type -}                            
-lines_val = TestCase ( do b  <- Data.ByteString.Lazy.readFile "line_plot/lines_val.json"
-                          assertEqual "Wrong input values" (parseFile b) (Errors [DataNotMatchesType "The input data does not match the required type. In the case of a line plot the input has to have type [[(x,y)]]"]))                          
+lines_val = TestCase ( do b  <- B.readFile "line_plot/lines_val.json"
+                          assertEqual "Wrong input values" (parseFile b) (Errors [DataNotMatchesType "The input data does not match the required type. In the case of a line plot the input has to have type [[(x,y)]]"]))         
+                          
+------------ XML tests ---------------
+
+{- Tests a correct xml file for a line plot with all fields filled -}
+xml_correct = TestCase (do b <- readFile "xml/line_full.xml"
+                           assertEqual "xml line plot" (parseXML' b) (Errors [NoErrorsFound "Found no errors"]))
+
+{- Test an empty xml file -}
+xml_empty = TestCase (do b <- readFile "xml/empty.xml"
+                         assertEqual "Empty file" (parseXML' b) (Errors [RequiredFieldMissing "One of the required fields has incorrect input or is missing."]))
+ 
+{- Tests an xml file where entry for the graph type is missing -}
+xml_missing = TestCase (do b <- readFile "xml/missing.xml"
+                           assertEqual "Missing graph type" (parseXML' b) (Errors [RequiredFieldMissing "One of the required fields has incorrect input or is missing."]))
